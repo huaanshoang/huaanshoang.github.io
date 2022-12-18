ec567b870b7951ba3e591bcbfa8cb6f97af3cc89
huaanshoang <119106419+huaanshoang@users.noreply.github.com>
2022-12-18 21:35:50 +0700

    Add files via upload

---
+++ talking/s1/talk_1_1.js
@@ -0,0 +1,27 @@
+const aListU=[];    
+const aListB=[];
+
+aListU[0]='Hello.';
+aListB[0]='Hello.';
+
+aListU[1]='David Clark.';
+aListB[1]='Linda Rivera.';
+
+aListU[2]='Are you a teacher';
+aListB[2]="No, I'm not.";
+
+aListU[3]='Are you a student';
+aListB[3]='Yes, I am.';
+
+aListU[4]='Are you from the United States';
+aListB[4]="No, I'm not.";
+
+aListU[5]="Where are you from";
+aListB[5]="I'm from Canada.";
+
+aListU[6]="Hello Kenji";
+aListB[6]="Hi, John. How are you";
+
+aListU[7]="Fine, thanks. And you";
+aListB[7]="Fine thanks.";
+
